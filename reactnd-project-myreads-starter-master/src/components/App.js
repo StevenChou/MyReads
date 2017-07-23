885c99c185d55df28bb4e6bdfce0440cd64a1ae3
Unknown <steven.chou.second@gmail.com>
2017-07-23 18:01:08 +0800

    feat: Main paga functions
    1. The main page shows 3 shelves for books.
    
    2. The main page shows a control that allows users to move books between shelves.
    
    3. When the browser is refreshed, the same information is displayed on the page.

--- reactnd-project-myreads-starter-master/src/components/App.js
+++ reactnd-project-myreads-starter-master/src/components/App.js
@@ -8,11 +8,40 @@ import Bookshelves from './Bookshelves'
 import './../../style/App.css'
 
 class BooksApp extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      myBooks: []
+    }
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((myBooks) => {
+      this.setState({ myBooks })
+    })
+  }
+
+  changeShelf = (shelf, book) => {
+    const myBooks = this.state.myBooks.map(b => {
+      if (b.id === book.id) {
+        b.shelf = shelf
+      }
+      return b
+    })
+
+    this.setState({ myBooks })
+
+    BooksAPI.update(book, shelf)
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <Bookshelves />
+          <Bookshelves 
+            myBooks={this.state.myBooks}
+            onChangeShelf={this.changeShelf} />
         )} />
         <Route exact path="/search" render={() => (
           <SearchBooks />
